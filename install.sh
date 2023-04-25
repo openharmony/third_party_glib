09c800eb66fed7c445c9d74f13cedfc8428c5291
sqwlly <shiqiwei4@huawei.com>
2023-04-25 19:40:22 +0800

    三方开源软件合规文件以及BUILD.gn Signed-off-by: s30029175 <shiqiwei4@huawei.com>
    Signed-off-by: sqwlly <shiqiwei4@huawei.com>
    Change-Id: If76a767da886f77dece5eb38cb413407204f6b5d

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation version 2.1
+# of the License.
+#
+# Copyright(c) 2023 Huawei Device Co., Ltd.
+
+set -e
+cd $1
+if [ -d "glib-2.68.1" ];then
+    rm -rf glib-2.68.1
+fi
+tar -xvf glib-2.68.1.tar.xz
+patch -p1 < $1/backport-lib-openharmony-glib.patch
+exit 0
