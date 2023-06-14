b43b1cf5bb72102e2a17021539efb0e7a6f5263b
sqwlly <shiqiwei4@huawei.com>
2023-06-14 12:06:34 +0800

    OAT整改 Signed-off-by: s30029175 <shiqiwei4@huawei.com>
    Signed-off-by: sqwlly <shiqiwei4@huawei.com>
    Change-Id: Ia243fd871884f0c3d544cb2a3b0c9fda567fddce

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation version 2.1
-# of the License.
-#
 # Copyright(c) 2023 Huawei Device Co., Ltd.
 
 set -e
